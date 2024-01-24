5a1b1d92952881d6ab2d6c0016c9e3e1bf6b9a9f
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-01-24 16:58:57 +0530

    back

--- restFramework/project1/py_client/basic.py
+++ restFramework/project1/py_client/basic.py
@@ -10,4 +10,4 @@ print(get_response.text) # print raw text response
 
 # HTTP REQUEST -> HTML
 # REST API HTTP REQUEST -> JSON(xml)
-print(get_response.json()) #python dictionary -- only works if get_response is a json objec (response of rest api http request)
+print(get_response.json()) #python dictionary -- only works if get_response is a json object (response of rest api http request)
