2bca09f7a9c743347259fc092da0711ae2371e6f
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-01-24 17:30:14 +0530

    getting data from restapi -- backend to client

--- restFramework/project1/py_client/basic.py
+++ restFramework/project1/py_client/basic.py
@@ -1,13 +1,15 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org"
-endpoint="https://httpbin.org/anything" # rest api
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org"
+# endpoint= "https://httpbin.org/anything" # rest api
+endpoint =  "http://127.0.0.1:8000/api"
 
-get_response = requests.get(endpoint) # http request
-print(get_response.text) # print raw text response
+get_response = requests.get(endpoint, json = {"query": "hello world"}) # http request
+# print(get_response.text) # print raw text response
+print(get_response.json()['message'])  # geeting data from restapi
 
 
-# HTTP REQUEST -> HTML
-# REST API HTTP REQUEST -> JSON(xml)
-print(get_response.json()) #python dictionary -- only works if get_response is a json object (response of rest api http request)
+# # HTTP REQUEST -> HTML
+# # REST API HTTP REQUEST -> JSON(xml)
+# print(get_response.json()) #python dictionary -- only works if get_response is a json object (response of rest api http request)
