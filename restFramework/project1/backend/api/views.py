1d48a2ece7f3df2ac22861c5c76ab013bc3122f1
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-01-25 12:43:44 +0530

    completed echo getdata

--- restFramework/project1/backend/api/views.py
+++ restFramework/project1/backend/api/views.py
@@ -5,6 +5,8 @@ def api_home(request, *args, **kwargs):
     # request -> httpRequest from django
     # print(dir(request))
     # request.body 
+    print('function called')
+    print('HTTP Method:', request.method)
     body = request.body # byte string of JSON data
     print(body)
     data = {}
@@ -13,5 +15,15 @@ def api_home(request, *args, **kwargs):
         data = json.loads(body)
     except:
         pass
-    print(data)
-    return JsonResponse({"message": "hii there, this is your Django API response!!"})
+    print(data.keys())
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    # content = request.content
+    # print(content, "= content")
+    url = request.get_full_path
+    print(url, "= url")
+    print(data['headers'], "= header")
+    print(data['content_type'], "= content_type")
+    print(request.GET, "= request.GET")
+    print(request.POST, "= request.POST")
+    return JsonResponse(data)
