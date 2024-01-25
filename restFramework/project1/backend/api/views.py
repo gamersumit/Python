6654beddf5f7a141e43cb903cacd3f069d806153
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-01-25 14:20:50 +0530

    django model instance to Dictionary

--- restFramework/project1/backend/api/views.py
+++ restFramework/project1/backend/api/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from product.models import Product
+from django.forms.models import model_to_dict
 import json
 
 def api_home(request, *args, **kwargs):
@@ -32,8 +33,12 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data :
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['price'] = model_data.price
-        data['content'] = model_data.content
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['price'] = model_data.price
+        # data['content'] = model_data.content
+        # <!----- instead ------->
+        data = model_to_dict(model_data)
+        # also 
+        # data = model_to_dict(model_data, fields = ['id', 'title']) # taking some part/keys of our model
     return JsonResponse(data)
