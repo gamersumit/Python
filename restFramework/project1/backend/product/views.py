8e8c1bea18c1be977ec3630178603c6b40a1d56e
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-01-29 12:12:36 +0530

    generic createapiview or post

--- restFramework/project1/backend/product/views.py
+++ restFramework/project1/backend/product/views.py
@@ -6,4 +6,24 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # redefine perform_create method that is automaticatly handled by generics
+    def perform_create(self, serilaizer):
+        title = serilaizer.validated_data.get('title')
+        newcontent = serilaizer.validated_data.get('content') or None
+        if not newcontent:
+            newcontent = title
+        
+        # modified perform_create method to set our content equals to title if content is not provided during post api call
+        # now save this data to database
+        serilaizer.save(content = newcontent) # default  is serializer.save() now we added old/ empty content to the newvontent
+
+
+
+product_create_view = ProductCreateAPIView.as_view()
 product_detail_view = ProductDetailAPIView.as_view()
--- restFramework/project1/py_client/details.py
+++ restFramework/project1/py_client/details.py
@@ -4,5 +4,5 @@ import requests
 endpoint =  "http://127.0.0.1:8000/api/product/1"
 
 
-get_response = requests.get(endpoint, params = {"abc": 123}, json={"title" :"titiled", "content": "hello world"}) # http request
+get_response = requests.get(endpoint) # http request
 print(get_response.json())
