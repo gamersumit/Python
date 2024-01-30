06ae73b767221124735b1c73bd6a38fc09993475
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-01-30 15:52:06 +0530

    viewsets and routers

---
+++ restFramework/project1/backend/cfehome/routers.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter 
+from product.viewsets import ProductViewSet, ProductGenericViewSet
+
+router = DefaultRouter()
+router.register('product', ProductGenericViewSet,
+                basename='products')
+
+print(router.urls)
+urlpatterns = router.urls
