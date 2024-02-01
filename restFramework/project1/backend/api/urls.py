968a512bf04524d7f0fa18c084032eba67322bd4
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-02-01 15:52:19 +0530

    jwt webtoken authentication with simplejwt

--- restFramework/project1/backend/api/urls.py
+++ restFramework/project1/backend/api/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from product.views import product_list_create_view
 from . import views
 urlpatterns = [
+    
     path('auth/', obtain_auth_token),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('product/', product_list_create_view, name='product-list'),
     path('', views.api_home, name = 'api_home'),
 ]
