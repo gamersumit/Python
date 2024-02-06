1484ea5d90014d737f6f7cb48359d8623ba410e6
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-02-06 16:53:11 +0530

    sending mails

--- restFramework/Implement/backend/account/views.py
+++ restFramework/Implement/backend/account/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import CustomUserSerializer
 from .models import CustomUser
 import requests
-
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 
 # Create your views here.
@@ -13,7 +15,28 @@ import requests
 class CustomUserRegistrationView(generics.CreateAPIView) :
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-  
+
+class SendMail(APIView) :
+    def post(self,request):
+        
+        email = request.data.get('to')
+        if not email:
+            return Response({'status': False, 'message': 'Missing recipient email'}, status=400)
+      
+        emailw = EmailMessage(
+            'testing subject', 
+            'testing email body, this message is from python',
+            settings.EMAIL_HOST_USER,
+            [email]
+        )
+
+        try :
+            emailw.send(fail_silently=False)
+            return Response({'status' : True, 'message' : 'Email sent successfully'})
+        
+        except Exception as e:
+            return Response({'status' : False, 'message' : str(e)}, status = 500)
+
 
 
 # SHORT NAMING :
