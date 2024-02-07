b7e94f1d8efe4a9a1401a3ac6afdd1238dbdba11
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-02-07 16:14:49 +0530

    implemented resend email verification

--- restFramework/Implement/backend/account/urls.py
+++ restFramework/Implement/backend/account/urls.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('register/', views.user_register_view, name = 'user_register'),
     path('verification/', views.email_verify_view, name = 'email_verify'),
+    path('sendverification/', views.send_verification_link_view, name = 'sendverification'),
     path('login/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
